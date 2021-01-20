afe7ae3e1c55aad1c957f1b0414ef8d5d8b86944
bingbing <875307054@qq.com>
2021-01-20 22:25:17 +0800

    调整活动活动分页；布局个人中心

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config={
   alianceKey:"04rNbDIGuBoYcsQn",//后台分配的allianceKey
   dev:{//开发环境
-    domain:"http://127.0.0.1:8080/api",//后台接口地址
+    domain:"http://127.0.0.1:8081",//后台接口地址
     qiniuDomain:"https://image.qiuhuiyi.cn",//七牛地址
     bgImage:"tmp/wx0f587d7c97a68e2b.o6zAJs3oh85Zb1lJE8oWix57vny0.91gGjMXALWNEf6b9dd803a7fe4bf5f75b6afd5705a73.jpg"//个人中心背景图片
   },
@@ -12,8 +12,8 @@ const config={
   }
 }
 
-const domain = config.prod.domain;
-//const domain = config.dev.domain;
+//const domain = config.prod.domain;
+const domain = config.dev.domain;
 
 
 module.exports = {
