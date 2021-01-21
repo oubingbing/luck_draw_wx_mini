5883b1ffe20fa35d171ad3ca733d60ac3a5b52b7
ouzhibing <ouzhibing@galaxymx.com>
2021-01-21 19:29:51 +0800

    进行活动参加

--- pages/home/detail/detail.js
+++ pages/home/detail/detail.js
@@ -23,7 +23,7 @@ Page({
       if(resDate.code == 0){
         this.setData({activity:resDate.data})
       }else{
-        thiswx.showToast({
+        wx.showToast({
           title: '请求失败',
           icon:"none"
         })
@@ -32,13 +32,17 @@ Page({
   },
 
   joinActivity:function(){
-    http.post(`/activity/join`, {id:this.id}, res => {
+    http.post(`/activity/join`, {id:this.data.id}, res => {
       let resDate = res.data
+      console.log(resDate)
       if(resDate.code == 0){
-        this.setData({activity:resDate.data})
+        wx.showLoading({
+          title: resDate.msg,
+          icon:"none"
+        })
       }else{
-        thiswx.showToast({
-          title: '请求失败',
+        wx.showToast({
+          title: resDate.msg,
           icon:"none"
         })
       }
