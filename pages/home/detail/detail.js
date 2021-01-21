0d1824400581e27c329a5a7fb263cbfd0dcd7c36
ouzhibing <ouzhibing@galaxymx.com>
2021-01-21 14:24:14 +0800

    完成详情

--- pages/home/detail/detail.js
+++ pages/home/detail/detail.js
@@ -1,65 +1,36 @@
-// pages/home/detail.js
+const http = require("./../../../utils/http.js");
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    id:"",
+    activity:null
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let id = options.id
+    this.setData({id:id})
+    this.getDetail()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getDetail:function(){
+    http.get(`/activity/detail?id=${this.data.id}`, {}, res => {
+      let resDate = res.data
+      if(resDate.code == 0){
+        this.setData({activity:resDate.data})
+      }else{
+        thiswx.showToast({
+          title: '请求失败',
+          icon:"none"
+        })
+      }
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   }
