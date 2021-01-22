ed7c304d66918461278112d379f43b71b5c33e56
ouzhibing <ouzhibing@galaxymx.com>
2021-01-22 19:55:59 +0800

    完成参加活动加入长连接

--- pages/home/detail/detail.js
+++ pages/home/detail/detail.js
@@ -1,20 +1,83 @@
 const http = require("./../../../utils/http.js");
 const app = getApp()
+let conn
 
 Page({
   data: {
     id:"",
-    activity:null
+    activity:null,
+    socketToken:"",
+    socketDomain:"",
   },
 
   onLoad: function (options) {
     let id = options.id
     this.setData({id:id})
     this.getDetail()
+    this.getSocketToken();
   },
 
   onReady: function () {
+    
+  },
 
+  connectSocket:function(){
+    conn = wx.connectSocket({
+      url: this.data.socketDomain+'/ws?token='+this.data.socketToken,
+      header:{
+        'content-type': 'application/json'
+      }
+    })
+
+    conn.onError(res=>{
+      console.log("socket错误提示：")
+      console.log(res)
+    })
+
+    conn.onOpen(res=>{
+      console.log(res)
+    })
+
+    conn.onClose(res=>{
+      console.log(res)
+    })
+
+    conn.onMessage(res=>{
+      let data = JSON.parse(res.data)
+      if(data){
+        if(data.code == 200){
+          wx.hideLoading()
+          let pushData = data.data
+          if (pushData.code == 0){
+            //加入成功
+            wx.showToast({
+              title: pushData.message,
+              icon:"success"
+            })
+          }else{
+            //加入失败
+            wx.showToast({
+              title: pushData.message,
+              icon:"none"
+            })
+          }
+        }
+      }
+    })
+  },
+
+  getSocketToken:function(){
+    http.get(`/socket/token`, {}, res => {
+      let resDate = res.data
+      if(resDate.code == 0){
+        this.setData({socketToken:resDate.data.token,socketDomain:resDate.data.domain})
+      }else{
+        wx.showToast({
+          title: '请求失败',
+          icon:"none"
+        })
+      }
+    });
   },
 
   getDetail:function(){
@@ -32,9 +95,12 @@ Page({
   },
 
   joinActivity:function(){
+    if(!conn){
+      this.connectSocket()
+    }
+    
     http.post(`/activity/join`, {id:this.data.id}, res => {
       let resDate = res.data
-      console.log(resDate)
       if(resDate.code == 0){
         wx.showLoading({
           title: resDate.msg,
