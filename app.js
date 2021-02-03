4fb84dc25e10a1bac1b4e5904e6e4c6a6f9a01c0
ouzhibing <ouzhibing@galaxymx.com>
2021-02-03 18:20:44 +0800

    调整页面布局

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ const config = require("./config.js");
 
 App({
   onLaunch: function () {
-
     this.globalData.apiUrl = config.domain;
     this.globalData.appKey = config.alianceKey;
     this.globalData.imageUrl = config.qiniuDomain+"/";
     this.globalData.bgIimage = config.bgImage;
     this.globalData.loadAddress = false
     this.globalData.activityId = 0
-    
     this.globalData.param = false;
     this.globalData.authStatus = false;
   },
-
   globalData: {
     appId:null,
     userInfo: null,
